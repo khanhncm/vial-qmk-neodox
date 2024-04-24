1023c2f1b57798d999f16c7dcf8d415a4271a8b6
khanhncm <76054890+khanhncm@users.noreply.github.com>
2024-04-25 02:35:47 +0700

    e to p

--- keyboards/newdox/keymaps/vial/keymap.c
+++ keyboards/newdox/keymaps/vial/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_8,   KC_1,    KC_2, KC_3,    KC_4, KC_5,                                          KC_P, KC_L,  KC_U,    KC_Y,    KC_F9,  KC_F10,
     KC_7,   KC_LSFT, KC_Q, KC_LSFT, KC_E, KC_T, KC_N,                        KC_EQL,  KC_J, KC_BTN1,    KC_WH_D,    KC_BTN2,     KC_WH_U,  KC_BSPC,
     KC_6,   KC_W,    KC_A, KC_S,    KC_D, KC_F, KC_G,                        KC_SLSH, KC_M, KC_MS_R,    KC_MS_D,    KC_MS_L,     KC_MS_U,    KC_ENT,
-    KC_ESC, KC_Z,    KC_X, KC_C,    KC_V, KC_B, KC_LALT, KC_LCTL,      KC_MSEL, KC_MAIL, KC_K, KC_H,    KC_P9s, KC_I,  KC_O, KC_APP,
+    KC_ESC, KC_Z,    KC_X, KC_C,    KC_V, KC_B, KC_LALT, KC_LCTL,      KC_MSEL, KC_MAIL, KC_K, KC_H,    KC_P, KC_I,  KC_O, KC_APP,
     KC_INS, KC_END,  KC_R, KC_DEL, KC_SPC,  KC_9, KC_0,                KC_RBRC, KC_BTN2,   KC_BTN1, KC_LALT, KC_LSFT, KC_LCMD,TO(0)
   ),
 
