0004e2bbc041ba8b4208ada02165c1258c3d7d96
khanhncm <76054890+khanhncm@users.noreply.github.com>
2024-07-07 14:27:42 +0700

    switch layer left side

--- keyboards/newdox/keymaps/vial/keymap.c
+++ keyboards/newdox/keymaps/vial/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P, KC_B, KC_MINS,                        KC_EQL,  KC_J,  KC_L,  KC_U,    KC_Y,    KC_DOT, KC_BSPC ,
     KC_SPC,  KC_A,    KC_R,    KC_S,    KC_T, KC_G, KC_BSLS,                        KC_SLSH, KC_M,  KC_N,  KC_E,    KC_I,    KC_O,  KC_ENT,
     KC_ESC,  KC_Z,    KC_X,    KC_C,    KC_D, KC_V, KC_NO, KC_NO,      KC_NO, KC_NO, KC_K,  KC_H,  KC_SCLN, KC_QUOT, KC_COMM, KC_DEL,
-    KC_GRV, KC_LCMD, KC_LSFT, KC_LALT, MO(2),    KC_LCTL,   KC_LBRC,      KC_RBRC, KC_LCTL,        MO(2), KC_LALT, KC_LSFT, KC_LCMD, KC_NO
+    KC_GRV, KC_LCMD, KC_LSFT, KC_LALT, MO(2),    KC_LCTL,   KC_LBRC,      KC_RBRC, KC_LCTL,        MO(2), KC_LALT, KC_LSFT, KC_LCMD, TO(1)
   ),
 
   [1] = LAYOUT(
@@ -32,14 +32,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_9, KC_LSFT, KC_Q, KC_LSFT, KC_E, KC_T, KC_N,                        KC_EQL,  KC_J, KC_BTN1,    KC_WH_D,    KC_BTN2,     KC_WH_U,  KC_BSPC,
     KC_8,   KC_W,    KC_A, KC_S,    KC_D, KC_F, KC_G,                        KC_SLSH, KC_M, KC_MS_R,    KC_MS_D,    KC_MS_L,     KC_MS_U,    KC_ENT,
     KC_ESC,   KC_Z,    KC_X, KC_C,    KC_V, KC_B, KC_LALT, MO(3),      KC_NO, KC_NO, KC_K, KC_H,    KC_P, KC_I,  KC_O, KC_DEL,
-    KC_GRV, KC_R,  KC_8, KC_7, KC_SPC,  KC_6, KC_LCTL,                KC_RBRC, KC_BTN2,   KC_BTN1, KC_LALT, KC_LSFT, KC_LCMD,KC_NO
+    KC_GRV, KC_R,  KC_8, KC_7, KC_SPC,  KC_6, KC_LCTL,                KC_RBRC, KC_BTN2,   KC_BTN1, KC_LALT, KC_LSFT, KC_LCMD, TO(0)
   ),
 
   [2] = LAYOUT(
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4, KC_F5,                                          KC_F6, KC_F7,    KC_F8,    KC_F9,     KC_F10,    KC_NO,
     KC_TAB,  KC_HOME, KC_PGUP, KC_END, KC_PGDN, KC_BSPC, KC_F11,                        KC_F12,  KC_J, KC_PGDN,    KC_END,    KC_PGUP,     KC_HOME,  KC_BSPC,
     KC_SPC,  KC_UP, KC_LEFT, KC_DOWN, KC_RIGHT, KC_ENT, KC_NO,                        KC_SLSH, KC_M, KC_RIGHT,  KC_DOWN,    KC_LEFT,     KC_UP,    KC_ENT,
-    KC_GRV,  KC_NO, KC_VOLD, KC_MUTE, KC_VOLU, KC_DEL, KC_NO, KC_NO,      KC_MSEL, KC_MAIL, KC_K,KC_VOLU,  KC_MUTE, KC_VOLD, KC_NO, KC_DEL,
+    KC_GRV,  KC_NO, KC_VOLD, KC_MUTE, KC_VOLU, KC_DEL, TO(1), KC_NO,      KC_MSEL, KC_MAIL, KC_K,KC_VOLU,  KC_MUTE, KC_VOLD, KC_NO, KC_DEL,
     KC_CAPS, KC_LCMD, KC_LSFT, KC_LALT, KC_NO,    KC_LCTL,   KC_LBRC,            KC_RBRC, KC_LCTL,       KC_NO, KC_LALT, KC_LSFT, KC_LCMD,   KC_NO
   ),
 
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_F4, KC_F5,                                          KC_F6, KC_F7,    KC_F8,    KC_F9,     KC_F10,    KC_NO,
     KC_F9,  KC_P, KC_O, KC_I, KC_U, KC_Y, KC_F11,                        KC_F12,  KC_J, KC_PGDN,    KC_END,    KC_PGUP,     KC_HOME,  KC_BSPC,
     KC_F8,  KC_INS, KC_L, KC_K, KC_J, KC_H, KC_F12,                        KC_SLSH, KC_M, KC_RIGHT,  KC_DOWN,    KC_LEFT,     KC_UP,    KC_ENT,
-    KC_NO,  KC_SLSH, KC_DOT, KC_COMM, KC_M, KC_V, KC_NO, KC_NO,      KC_MSEL, KC_MAIL, KC_K,KC_VOLU,  KC_MUTE, KC_VOLD,KC_NO, KC_DEL,
+    KC_NO,  KC_SLSH, KC_DOT, KC_COMM, KC_M, KC_V, TO(0), KC_NO,      KC_MSEL, KC_MAIL, KC_K,KC_VOLU,  KC_MUTE, KC_VOLD,KC_NO, KC_DEL,
     KC_CAPS, KC_LCMD, KC_F8, KC_F7, KC_NO,    KC_F6,   KC_LBRC,            KC_RBRC, KC_LCTL,       KC_NO, KC_LALT, KC_LSFT, KC_LCMD,   KC_NO
   )
 
