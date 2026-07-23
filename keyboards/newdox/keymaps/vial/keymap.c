11fb6324c13ab909ac27f9799f02f02b1a07dfce
khanhncm <khanhncmcom@gmail.com>
2026-07-23 13:19:32 -0400

    KC_TAB r1

--- keyboards/newdox/keymaps/vial/keymap.c
+++ keyboards/newdox/keymaps/vial/keymap.c
@@ -36,11 +36,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [2] = LAYOUT(
-    KC_TAB, KC_ESC,  KC_2, KC_3,    KC_4,   KC_5,                                          KC_P, KC_L,  KC_U,    KC_Y,    KC_F9,  TO(0),
-    KC_6,   KC_LSFT, KC_E, KC_LSFT, KC_E,   KC_R,    KC_T,                        KC_EQL,  KC_J, KC_BTN1,    KC_WH_D,    KC_BTN2,     KC_WH_U,  KC_BSPC,
-    KC_1,   KC_W,    KC_A, KC_S,    KC_D,   KC_F,    KC_G,                        KC_SLSH, KC_M, KC_MS_R,    KC_MS_D,    KC_MS_L,     KC_MS_U,    KC_ENT,
-    KC_7,   KC_Z,    KC_X, KC_C,    KC_V,   KC_B,    KC_N, MO(3),      KC_NO, KC_NO, KC_K, KC_H,    KC_P, KC_I,  KC_O, KC_DEL,
-    KC_Q,   KC_8,    KC_9, KC_0,    KC_SPC, KC_LCTL, KC_LALT,                KC_RBRC, KC_BTN2,   KC_BTN1, KC_LALT, KC_LSFT, KC_LCMD, KC_NO
+    KC_8, KC_1,  KC_2, KC_3,    KC_4,   KC_5,                                          KC_P, KC_L,  KC_U,    KC_Y,    KC_F9,  TO(0),
+    KC_7,   KC_LSFT, KC_E, KC_LSFT, KC_E,   KC_R,    KC_T,                        KC_EQL,  KC_J, KC_BTN1,    KC_WH_D,    KC_BTN2,     KC_WH_U,  KC_BSPC,
+    KC_6,   KC_W,    KC_A, KC_S,    KC_D,   KC_F,    KC_G,                        KC_SLSH, KC_M, KC_MS_R,    KC_MS_D,    KC_MS_L,     KC_MS_U,    KC_ENT,
+    KC_ESC, KC_Q,    KC_X, KC_C,    KC_V,   KC_B,    KC_N, MO(3),      KC_NO, KC_NO, KC_K, KC_H,    KC_P, KC_I,  KC_O, KC_DEL,
+    KC_Z,   KC_9,    KC_0, KC_TAB,   KC_SPC, KC_LCTL, KC_LALT,                KC_RBRC, KC_BTN2,   KC_BTN1, KC_LALT, KC_LSFT, KC_LCMD, KC_NO
   ),
 
   [3] = LAYOUT(
