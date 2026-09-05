d39523ec0aeabaa66e1ff2d3a4484a81f4a10ffb
khanhncm <khanhncmcom@gmail.com>
2026-09-05 11:46:12 -0400

    update . dragonsword, sb works

--- keyboards/newdox/keymaps/vial/keymap.c
+++ keyboards/newdox/keymaps/vial/keymap.c
@@ -1,19 +1,3 @@
- /* Copyright HarshitGoel96 2020
-  * With permission from mattdibi, the original maintainer of the Redox hardware.
-  * 
-  * This program is free software: you can redistribute it and/or modify 
-  * it under the terms of the GNU General Public License as published by 
-  * the Free Software Foundation, either version 2 of the License, or 
-  * (at your option) any later version. 
-  * 
-  * This program is distributed in the hope that it will be useful, 
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-  * GNU General Public License for more details. 
-  * 
-  * You should have received a copy of the GNU General Public License 
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
-  */ 
 #include QMK_KEYBOARD_H
 #include "oled.c"
 #include "encoder.c"
@@ -28,11 +12,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [1] = LAYOUT(
-    KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_F4, KC_F5,                                          KC_F6, KC_KP_MINUS, KC_KP_ASTERISK, KC_KP_SLASH, KC_NUM_LOCK, KC_NO,
-    KC_F9,  KC_6,    KC_7,    KC_8,  KC_9, KC_BSPC, KC_F11,                        KC_F12,  KC_J, KC_KP_9,    KC_KP_8,    KC_KP_7,     KC_KP_PLUS,  KC_BSPC,
-    KC_F8,  KC_UP, KC_LEFT, KC_DOWN, KC_RIGHT, KC_ENT, KC_F12,                        KC_SLSH, KC_M, KC_KP_6,  KC_KP_5,    KC_KP_4,     KC_KP_DOT,    KC_ENT,
-    KC_F7,  KC_0, KC_COMM, KC_DOT, KC_SLSH, KC_DEL, TO(2), TO(4),      KC_NO, KC_NO, KC_K,KC_KP_3,  KC_KP_2, KC_KP_1, KC_KP_0, KC_DEL,
-    KC_F6, KC_LCMD, KC_LSFT, KC_LALT, KC_NO,    KC_LCTL,   KC_LBRC,            KC_RBRC, KC_LCTL,       KC_NO, KC_LALT, KC_LSFT, KC_LCMD,   KC_NO
+    KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                         KC_F6,   KC_KP_MINUS, KC_KP_ASTERISK, KC_KP_SLASH, KC_NUM_LOCK, KC_NO,
+    KC_F9,   KC_6,    KC_7,    KC_8,    KC_9,    KC_BSPC, KC_F11,                       KC_F12, KC_WH_U, KC_KP_9,      KC_KP_8,        KC_KP_7,       KC_KP_PLUS,  KC_BSPC,
+    KC_F8,   KC_UP,   KC_LEFT, KC_DOWN, KC_RIGHT,KC_ENT,  KC_F12,                       KC_SLSH,KC_WH_D, KC_KP_6,      KC_KP_5,        KC_KP_4,       KC_KP_DOT,   KC_ENT,
+    KC_F7,   KC_0,    KC_COMM, KC_DOT,  KC_SLSH, KC_DEL,  TO(2),  TO(4),      KC_NO, KC_NO, KC_K,   KC_KP_3, KC_KP_2,      KC_KP_1,        KC_KP_0,       KC_DEL,
+    KC_F6,   KC_LGUI, KC_LSFT, KC_LALT, KC_NO,   KC_LCTL, KC_LBRC,                         KC_RBRC,KC_LCTL, KC_NO,        KC_LALT,        KC_LSFT,       KC_LGUI,     KC_NO
   ),
 
   [2] = LAYOUT(
@@ -52,19 +36,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [4] = LAYOUT(
-    KC_ESC, KC_1,    KC_2, KC_3,    KC_4, KC_5,                                          KC_P, KC_L,  KC_U,    KC_Y,    KC_F9,  TO(0),
-    KC_8,   KC_LSFT, KC_Q, KC_LSFT, KC_E, KC_F, KC_INS,                        KC_EQL,  KC_J, KC_BTN1,    KC_WH_D,    KC_BTN2,     KC_WH_U,  KC_BSPC,
-    KC_7,   KC_W,    KC_A, KC_S,    KC_D,  KC_T, KC_END,                        KC_SLSH, KC_M, KC_MS_R,    KC_MS_D,    KC_MS_L,     KC_MS_U,    KC_ENT,
-    KC_6,   KC_Z,    KC_X, KC_C,    KC_V,  KC_B, KC_LALT, MO(5),      KC_WH_U, KC_WH_D, KC_K, KC_H,    KC_P, KC_I,  KC_O, KC_DEL,
-    KC_TAB, KC_9,    KC_0, KC_G,    KC_SPC, KC_N, KC_END,                KC_RBRC, KC_BTN2,   KC_BTN1, KC_LALT, KC_LSFT, KC_LCMD, KC_NO
+    KC_ESC,  KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5,                                                    KC_P,    KC_L,    KC_U,    KC_Y,    KC_F9,   TO(0),
+    KC_TAB,  KC_R,    KC_J,    KC_F,    KC_E,    KC_T,    KC_KP_7,                                  KC_EQL,  KC_J,    KC_BTN1, KC_WH_D, KC_BTN2, KC_WH_U, KC_BSPC,
+    KC_Q,    KC_W,    KC_A,    KC_S,    KC_D,    KC_G,    KC_KP_6,                                  KC_SLSH, KC_M,    KC_MS_R, KC_MS_D, KC_MS_L, KC_MS_U, KC_ENT,
+    KC_KP_0, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LALT, MO(5),                  KC_WH_U, KC_WH_D, KC_K,    KC_H,    KC_P,    KC_I,    KC_O,    KC_DEL,
+    KC_CAPS, KC_LGUI, KC_LSFT, KC_LALT, KC_SPC,  KC_LCTL, KC_KP_1,                                  KC_RBRC, KC_BTN1, KC_BTN2, KC_LALT, KC_LSFT, KC_LGUI, KC_NO
   ),
 
   [5] = LAYOUT(
-    KC_GRV,  KC_F1,  KC_F2,   KC_F3, KC_F4, KC_F5,                                          KC_F6, KC_F7,    KC_F8,    KC_F9,     KC_F10,    KC_NO,
-    KC_F8,   KC_P,    KC_Y,   KC_U,    KC_L,  KC_J,  KC_F11,                        KC_F12,  KC_J, KC_PGDN,    KC_END,    KC_PGUP,     KC_HOME,  KC_BSPC,
-    KC_F7,   KC_O,    KC_I,   KC_P,    KC_R,  KC_M,  KC_F12,                        KC_SLSH, KC_M, KC_RIGHT,  KC_DOWN,    KC_LEFT,     KC_UP,    KC_ENT,
-    KC_F6,   KC_SLSH, KC_DOT, KC_COMM, KC_H,  KC_K, TO(0), KC_NO,      KC_MSEL, KC_MAIL, KC_K,KC_VOLU,  KC_MUTE, KC_VOLD,KC_NO, KC_DEL,
-    KC_CAPS, KC_F9,   KC_F10, KC_F7,   KC_NO, KC_F6, KC_LBRC,            KC_RBRC, KC_LCTL,       KC_NO, KC_LALT, KC_LSFT, KC_LCMD,   KC_NO
+    KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_NO,
+    KC_F9,   KC_P,    KC_Y,    KC_U,    KC_L,    KC_J,    KC_F11,                       KC_F12, KC_J,    KC_PGDN, KC_END,  KC_PGUP, KC_HOME, KC_BSPC,
+    KC_F8,   KC_O,    KC_I,    KC_P,    KC_R,    KC_M,    KC_F12,                       KC_SLSH,KC_M,    KC_RIGHT,KC_DOWN, KC_LEFT, KC_UP,   KC_ENT,
+    KC_F7,   KC_SLSH, KC_DOT,  KC_COMM, KC_H,    KC_K,    TO(0),   KC_NO,      KC_MSEL, KC_MAIL,KC_K,   KC_VOLU, KC_MUTE, KC_VOLD, KC_NO,   KC_DEL,
+    KC_F6,   KC_F9,   KC_F10,  KC_F7,   KC_NO,   KC_F6,   KC_LBRC,                         KC_RBRC,KC_LCTL, KC_NO,   KC_LALT, KC_LSFT, KC_LGUI, KC_NO
   )
 
 };
